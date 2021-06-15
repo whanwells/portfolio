8b643f755d55c8be19451eac4dc63e111a0ac157
William Wells <whanwells@gmail.com>
2021-06-14 23:36:24 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [
+    './src/**/*.njk'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
